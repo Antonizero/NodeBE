8fb414b321abfda75a5fe01d38882601106c89e5
Antonio Guiotto <powerhydratoni@gmail.com>
2020-07-11 13:22:35 +0200

    get and post

--- index.js
+++ index.js
@@ -1,9 +1,44 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
+
+const Todo = require('./model')
+
 const app = express();
 
 app.use(bodyParser.json());
 
-app.listen(process.env.PORT);
+app.get('/', (req, res, next) => {
+    Todo.find({}, (err, todos) => {
+        if (!err) {
+            res.status(200).json(todos)
+            next()
+        } else {
+            res.status(400).json({message: 'something went wrong when getting all todos'})
+            next()
+        }
+    });
+})
+
+app.post('/todo', (req, res, next) => {
+    const { title, body } = req.body;
+    console.log({title, body});
+    if (title && body) {
+        const todo = new Todo({title, body})
+        todo.save()
+        res.status(200).json({message: 'you succesfully connect to the server and wrote to database'});
+        next();
+    } else {
+        res.status(400).json({message: 'something went wrong when posting todo'});
+        next()
+    }
+})
+
+mongoose.connect(process.env.DATABASE, { useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => {
+        console.log('database connected')
+        app.listen(process.env.PORT)
+    })
+    .catch(error => console.log(error));
+
